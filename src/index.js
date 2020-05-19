d1e8af3f0e778648efb19cdf0d1ee0a06c0f5e2d
Amferiel <incanu94@mail.com>
2020-05-20 00:22:51 +0300

    Created pug-code for feature. #6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import $ from 'jquery';
 import './assets/scss/fonts.scss';
+
+import './blocks/btn/btn';
+import './blocks/navigation/navigation';
 import './blocks/header/header';
 import './pages/index';
 
