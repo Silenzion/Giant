59a670cc4469aaeb9128711baf6a751dbe77d6e2
Amferiel <incanu94@mail.com>
2020-05-20 14:53:10 +0300

    Renaming the btn into button. #11

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import './assets/scss/fonts.scss';
 
-import './blocks/btn/btn';
+import './blocks/button/button';
 import './blocks/navigation/navigation';
 import './blocks/header/header';
 import './pages/index';
